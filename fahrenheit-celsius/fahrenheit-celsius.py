60a77482c37f2e5e1d8dc41fd41704c9e17798b5
illousky <ignacio.gonzalezporras@estudiante.uam.es>
2025-06-24 19:27:07 +0200

    creando mi primer modelo de ia. Conversor de fahrenheit a celsius

---
+++ fahrenheit-celsius/fahrenheit-celsius.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+import numpy as np
+
+# Datos para el entrenamiento
+fahrenheit = np.array([-40, -10, 0, 32, 70, 100, 212], dtype=float)
+celsius = np.array([-40, -23.3, -17.8, 0, 21.1, 37.8, 100], dtype=float)
+nuevo_modelo = False # Variable para indicar si se crea un nuevo modelo
+
+modelo = tf.keras.models.load_model('modelo_fahrenheit_a_celsius.h5')
+# Si el modelo ya existe, se carga. Si no, se crea uno nuevo.
+
+# Definición del modelo si no se carga
+if modelo is None:
+    modelo = tf.keras.Sequential([
+        tf.keras.Input(shape=(1,)),  # Entrada de un solo valor
+        tf.keras.layers.Dense(units=1) # Capa densa con una sola neurona
+    ])
+
+    nuevo_modelo = True
+
+# Compilación del modelo si no se carga
+if not modelo.built:
+    modelo.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
+        loss='mean_squared_error'
+    )
+
+# Entrenamiento del modelo si no se carga
+if not modelo.optimizer:
+    historial = modelo.fit(fahrenheit, celsius, epochs=1000, verbose=False)
+
+# Probar el modelo con un valor nuevo
+resultado = modelo.predict(np.array([100.0]))
+print("100°F son aproximadamente %.2f°C" % resultado[0][0]) # Se usa resultado[0][0] para acceder al valor predicho ya que es un np.array
+
+
+# Visualización del historial de entrenamiento si el modelo es nuevo
+if nuevo_modelo:
+    import matplotlib.pyplot as plt
+
+    plt.plot(historial.history['loss'])
+    plt.title('Errores durante el entrenamiento')
+    plt.xlabel('Numero de rondas de entrenamiento (epochs)')
+    plt.ylabel('Pérdida (loss)')
+    plt.grid(True)
+    plt.show()
+
+
+# Imprimir los pesos del modelo
+capa = modelo.layers[0]
+pesos = capa.get_weights()
+print("Peso (multiplicador):", pesos[0][0])
+print("Sesgo (bias):", pesos[1][0])
+
+# Guardar el modelo si es nuevo
+if nuevo_modelo:
+    modelo.save('modelo_fahrenheit_a_celsius.h5')
